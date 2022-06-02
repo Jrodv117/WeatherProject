dbb74fbfafe2c5ee4817dffa00ad75436c020923
jrodr117 <jrodr113@msudenver.edu>
2022-06-02 17:56:07 -0600

    used nodes native https module to .get data from a weather api, parsed it and obtained two pieces of data from the parsed object

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+const { response } = require('express');
 const express = require('express');
 const https = require('https');
 
 const app = express();
 
-app.get('/', function (req, res) {
-	https.get();
+app.get('/', (req, res) => {
+	//storing the url inside a variable named url for ease of use
+	const url =
+		'https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=1a4a046cf6908026293343eb6fa7250b&units=imperial';
+	// using nodes native https module to get a response from the url
+	https.get(url, (urlresponse) => {
+		// taking the url response and getting the data from in
+		urlresponse.on('data', (data) => {
+			//console logs data which returns a ton of hexadecimals
+			// console.log(data);
+			// using JSON.parse and passing data so we get back a js object and not just hexadecimal data
+			const weatherData = JSON.parse(data);
+			//console logs weatherData and returns a js object
+			// console.log(weatherData);
+			// takes a specific piece of data from weatherData thats stored in .main.temp
+			const temp = weatherData.main.temp;
+			//console log to make sure we have the correct piece of data
+			// console.log(temp);
+			// grabs weather description from weather data
+			const weatherDescription = weatherData.weather[0].description;
+			// console.logs that description to make sure we have the right piece of data
+			console.log(weatherDescription);
+		});
+	});
 	// res.send(__dirname + '/index.html');
 	res.send('running');
 });
