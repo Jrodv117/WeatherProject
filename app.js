7505f6e61217542f4aa0ceefcc8498952c44e7b9
jrodr117 <jrodr113@msudenver.edu>
2022-06-03 13:13:30 -0600

    used body-parser module to allow a post method from a form so that user has the ability to choose location for weaher query. Also key is in active so please replace my key wiht yours

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const { response } = require('express');
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
 	//storing the url inside a variable named url for ease of use
+	const query = req.body.cityName;
 	const url =
-		'https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=1a4a046cf6908026293343eb6fa7250b&units=imperial';
+		'https://api.openweathermap.org/data/2.5/weather?q=' +
+		query +
+		'&appid=1a4a046cf6908026293343eb6fa7250b&units=imperial';
 	// using nodes native https module to get a response from the url
 	https.get(url, (urlresponse) => {
 		// taking the url response and getting the data from in
@@ -25,13 +35,19 @@ app.get('/', (req, res) => {
 			// grabs weather description from weather data
 			const weatherDescription = weatherData.weather[0].description;
 			// console.logs that description to make sure we have the right piece of data
-			console.log(weatherDescription);
+			// console.log(weatherDescription);
+			const icon = weatherData.weather[0].icon;
+			const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+
+			res.write('<p>the weather is currently ' + weatherDescription + '</p>');
+			res.write(
+				'<h1>The temperature in ' + query + ' is ' + temp + 'degrees</h1>'
+			);
+			res.write('<img src=' + imageURL + ' />');
+			res.send();
 		});
 	});
-	// res.send(__dirname + '/index.html');
-	res.send('running');
 });
-
 app.listen(3000, function () {
 	console.log('server is running on port 3000');
 });
